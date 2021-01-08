8e4d1709bc3f133d7684fde0ced9501c8acf506c
walter-rothlin <walter@rothlin.com>
2021-01-08 06:31:48 +0100

    Update pythonBasics_12_MetaDataInPicture.py

--- Python_WaltisExamples/Code_02_BasicPython/pythonBasics_12_MetaDataInPicture.py
+++ Python_WaltisExamples/Code_02_BasicPython/pythonBasics_12_MetaDataInPicture.py
@@ -1,10 +1,22 @@
-# http://metapicz.com/#landing  Web-applikation zum Meta-Tags auslesen
+# http://metapicz.com/#landing  Web-Applikation zum Meta-Tags auslesen
+
 
 import PIL.Image
+
 img = PIL.Image.open('Vogt-7.JPG')
 exif_data = img._getexif()
+print(exif_data)
 
+
+
+
+
+
+
+
+
+# JSON besser formatieren
 for anEntryKey in exif_data:
     print(anEntryKey, "-->", exif_data[anEntryKey])
 
-print(exif_data)
+
