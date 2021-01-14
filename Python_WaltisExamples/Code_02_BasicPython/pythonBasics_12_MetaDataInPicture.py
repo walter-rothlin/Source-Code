2abf4aa01a242c42c85ee92afc040fe2538cead7
walter-rothlin <walter@rothlin.com>
2021-01-14 15:05:56 +0100

    Update pythonBasics_12_MetaDataInPicture.py

--- Python_WaltisExamples/Code_02_BasicPython/pythonBasics_12_MetaDataInPicture.py
+++ Python_WaltisExamples/Code_02_BasicPython/pythonBasics_12_MetaDataInPicture.py
@@ -1,11 +1,17 @@
+# https://pillow.readthedocs.io/en/stable/reference/Image.html#examples    Bildverarbeitung
 # http://metapicz.com/#landing  Web-Applikation zum Meta-Tags auslesen
 
-import PIL.Image
+from PIL import Image
 
-img = PIL.Image.open('Vogt-7.JPG')
+img = Image.open('Vogt-7.JPG')
 exif_data = img._getexif()
 print(exif_data)
 
 
 
+# JSON besser formatieren
+for anEntryKey in exif_data:
+    print(anEntryKey, "-->", exif_data[anEntryKey])
 
+
+img.rotate(45).show()
