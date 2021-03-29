75d070bffaf41980e433856007a29c9b23edea5f
walter-rothlin <walter@rothlin.com>
2021-03-29 21:57:13 +0200

    Create linie_06a.py

---
+++ Python_WaltisExamples/Code_03_HAT/linie_06a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+# Linien zeichnen
+# ---------------
+
+from sense_hat import SenseHat
+from time import sleep
+
+sense = SenseHat()
+sense.clear()
+
+#roter Punkt Mitte horizontal Ping-Pong
+pos = 0
+direction = 1
+while True:
+	sense.clear()
+	if direction > 0:
+		sense.set_pixel(pos, 3, 255, 0, 0)
+	else:
+		sense.set_pixel(pos, 3, 0, 0, 255)
+	pos += direction
+	if pos > 7:
+		pos = 6
+		direction *= -1
+	if pos < 0:
+		pos = 1
+		direction *= -1
+	sleep(0.5)
+
