a6e02f013a7ebfa7e4c0efaa7d0a22a8ca552e84
walter-rothlin <walter@rothlin.com>
2021-04-13 15:13:50 +0200

    Update meteoStation.py

--- Python_WaltisExamples/Code_03_HAT/meteoStation.py
+++ Python_WaltisExamples/Code_03_HAT/meteoStation.py
@@ -10,33 +10,23 @@
 #
 # History:  
 # 10-Apr-2018	Walter Rothlin		Initial Version
+# 13-Apr-2021   Walter Rothlin		Refactoring
 # ------------------------------------------------------------------
 
 from sense_hat import SenseHat
 from time import sleep
-from support import *
-import math
 
+
+# Start Main  (Hauptprogramm)
 sense = SenseHat()
-
-while (True):
-	humidity = sense.get_humidity()
-	feuchteStr ="Humidity: {feuchte:3.1f}%".format(feuchte=humidity)
-
-	temp = sense.get_temperature()
-	tempStr = "Temperature: {temperatur:3.1f}C".format(temperatur=temp)
-
-	pressure = sense.get_pressure()
-	pressStr = "Pressure: {druck:3.1f}mBar".format(druck=pressure)
-
-	meteoStr = feuchteStr + "    " + tempStr + "    " + pressStr
-	print(meteoStr)
-	
-	sense.show_message(meteoStr)
-	tempStr = "{temperatur:3.0f}C".format(temperatur=temp)
-	for y in range(1,5):
-		sense.show_message(tempStr,scroll_speed=0.5,back_colour=[125,0,0],text_colour=[0,125,0])
-	sleep(1)
+while True:
+	sense.clear()
+	temp = round(sense.get_temperature(), 2)
+	feuchte = sense.get_humidity()
+	druck = sense.get_pressure()
+	displyStr = "Temp:{T:4.1f}C Feuchte:{Fe:3.0f}% Druck:{druck:3.0f}mbar".format(Fe=feuchte, T=temp, druck=druck)
+	print(displyStr)
+	sense.show_message(displyStr, scroll_speed=0.2, back_colour=[125, 0, 0], text_colour=[0, 125, 0])
 
 
 
