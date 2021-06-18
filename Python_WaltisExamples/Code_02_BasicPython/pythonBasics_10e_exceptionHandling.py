3c25234f5eaa1f0b0aa8c41823bbe41cee51414c
walter-rothlin <walter@rothlin.com>
2021-06-18 13:50:13 +0200

    Update pythonBasics_10e_exceptionHandling.py

--- Python_WaltisExamples/Code_02_BasicPython/pythonBasics_10e_exceptionHandling.py
+++ Python_WaltisExamples/Code_02_BasicPython/pythonBasics_10e_exceptionHandling.py
@@ -14,7 +14,32 @@
 # 24-Jun_2021   Walter Rothlin      Use of own library
 # ------------------------------------------------------------------
 from waltisLibrary import *
+import matplotlib.pyplot as plt
+import numpy as np
 
+def drawParabel(a, b, c):
+    # 100 linearly spaced numbers
+    x = np.linspace(-10, 10, 100)
+
+    # the function
+
+    y = ((a * (x**2)) + (b*x)) + c
+
+    # setting the axes at the centre
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.spines['left'].set_position('center')
+    ax.spines['bottom'].set_position('zero')
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+
+    # plot the function
+    plt.plot(x, y, 'r')
+
+    # show the plot
+    plt.show()
 
 # ====
 # Main
@@ -32,6 +57,7 @@ c = readFloat("c=")
 print("0 = ax^2 + bx + c  x12")
 loesungen = calcNullstellen(a, b, c)
 print(loesungen)
+drawParabel(a, b, c)
 
 
 
