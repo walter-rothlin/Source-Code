7a2fb219a24670c98a03c8da36b30f1ea2a0c655
walter-rothlin <walter@rothlin.com>
2021-07-04 06:03:12 +0200

    update

--- Datenbanken/Scripts/Create_Bankkonto.sql
+++ Datenbanken/Scripts/Create_Bankkonto.sql
@@ -20,9 +20,11 @@ USE `MyBank`;
 DROP TABLE IF EXISTS bankkonto;
 CREATE TABLE IF NOT EXISTS bankkonto (
   id_bankkonto int(11) NOT NULL AUTO_INCREMENT,
-  saldo double NOT NULL DEFAULT '0',
+  saldo double NOT NULL DEFAULT 0,
   PRIMARY KEY (id_bankkonto)
-) AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+);
+
+INSERT INTO `bankkonto` VALUES (1,2000),(2,5000);
 
 
-INSERT INTO `bankkonto` VALUES (1,1000),(2,35050),(3,200),(4,36000),(5,9);
+-- INSERT INTO `bankkonto` VALUES (1,1000),(2,35050),(3,200),(4,36000),(5,9);
--- Python_WaltisExamples/Code_08_MySql/05_Transactions.py
+++ Python_WaltisExamples/Code_08_MySql/05_Transactions.py
@@ -1,30 +1,82 @@
 #!/usr/bin/python3
 
 from waltisLibrary import *
-import sys
 import mysql.connector
 
 # https://pynative.com/python-mysql-transaction-management-using-commit-rollback/
 
+def showSaldo(title="", titleLevel=1):
+
+    if titleLevel == 1:
+        uChar = "="
+        abstand = "\n"
+    elif titleLevel == 2:
+        uChar = "-"
+        abstand = ""
+
+    print(unterstreichen(title, uChar), abstand)
+    # zeige Konto Saldos and Bilanzsumme
+    sql_show_saldo_query = """
+    select id_bankkonto, saldo from bankkonto;
+    """
+    cursor.execute(sql_show_saldo_query)
+    myresult = cursor.fetchall()
+    print("+------+--------------+")
+    print("| Id   | Saldo        |")
+    print("+------+--------------+")
+    bilanzsumme = 0
+    for aRec in myresult:
+        bilanzsumme += aRec[1]
+        print("| {id:4d} |".format(id=aRec[0]), end="")
+        print(" {saldo:12.2f} |".format(saldo=aRec[1]), end="")
+        print()
+    print("+------+--------------+")
+    print("| Summe: {summe:12.2f} |".format(summe=bilanzsumme))
+    print("+------+--------------+")
+    print("Records found:", len(myresult), myresult)
+
+
+dbServer = "localhost"
+dbSchema = "MyBank"
+userName = "root"
+password = "admin"
+
 try:
-    conn = mysql.connector.connect(host='localhost',
-                                   database='python_db',
-                                   user='pynative',
-                                   password='pynative@#29')
+
+    print(f"Connecting to '{dbSchema:s}' with user '{userName:s}'....", end="", flush=True)
+    # https://dev.mysql.com/doc/connector-python/en/connector-python-connectargs.html
+    conn = mysql.connector.connect(
+        host=dbServer,
+        database=dbSchema,
+        user=userName,
+        passwd=password
+    )
+    print("completed!")
 
     conn.autocommit = False
     cursor = conn.cursor()
-    # withdraw from account A
-    sql_update_query = """Update account_A set balance = 1000 where id = 1"""
+
+    showSaldo("Bevor Kontoübertrag")
+
+    # withdraw from Lohnkonto
+    withdrawAmount = readFloat("Betrag: ", min=10, max=10000)
+    sql_update_query = """Update bankkonto set saldo = saldo - """ + str(withdrawAmount) + """ where id_bankkonto = 1"""
     cursor.execute(sql_update_query)
 
-    # Deposit to account B
-    sql_update_query = """Update account_B set balance = 1500 where id = 2"""
+    showSaldo("Abgebucht aber noch nicht eingebucht", 2)
+    halt("Check with workebnch!! Than press RETURN to continue!")
+
+
+    # deposit to Sparkonto
+    sql_update_query = """Update bankkonto set saldo = saldo + """ + str(withdrawAmount) + """ where id_bankkonto = 2"""
     cursor.execute(sql_update_query)
-    print("Record Updated successfully ")
+    print("Record Updated successfully...", end="", flush=True)
 
     # Commit your changes
     conn.commit()
+    print("and commited!")
+
+    showSaldo("Nach Kontoübertrag")
 
 except mysql.connector.Error as error:
     print("Failed to update record to database rollback: {}".format(error))
@@ -35,4 +87,4 @@ finally:
     if conn.is_connected():
         cursor.close()
         conn.close()
-        print("connection is closed")
+        print("Connection is closed")
