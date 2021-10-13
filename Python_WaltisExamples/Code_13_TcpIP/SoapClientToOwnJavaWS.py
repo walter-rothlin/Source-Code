1d61f8933c6561ad528f68e5ce4ddc7469b597ac
walter-rothlin <walter@rothlin.com>
2021-10-13 10:19:59 +0200

    Create SoapClientToOwnJavaWS.py

---
+++ Python_WaltisExamples/Code_13_TcpIP/SoapClientToOwnJavaWS.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+# ------------------------------------------------------------------
+# Name: SoapClientToOwnJavaWS.py
+#
+# Description: Calls a WS witten in Java (JEE)
+#
+# Autor: Walter Rothlin
+#
+# History:
+# 12-Oct-2021   Walter Rothlin      Initial Version
+# ------------------------------------------------------------------
+import zeep
+
+# Client config using WSDL
+client = zeep.Client(wsdl='http://desktop-nskdqsg:8080/04_a_Webservice_Simple/FirstWebServiceService?wsdl')
+
+# Calling Web-Services and display result
+print("Pin()              :", client.service.ping())
+print('sayHelloName("HWZ"):', client.service.sayHelloName("HWZ"))
+print('calcCircleArea(5)  :', client.service.calcCircleArea(5))
