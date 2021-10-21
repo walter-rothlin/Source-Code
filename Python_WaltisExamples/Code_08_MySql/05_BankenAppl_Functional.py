bb9801274db0410f2a94baa056373b8aef4e896d
walter-rothlin <walter@rothlin.com>
2021-10-21 10:57:02 +0200

    Update

---
+++ Python_WaltisExamples/Code_08_MySql/05_BankenAppl_Functional.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python3
+
+# ------------------------------------------------------------------
+# Name: 05_BankenAppl_Functional.py
+#
+# Description: Implements Kontoübertrag (Lösung mit Functions)
+#
+#              Use the following sql-script to create MyBank schema:
+#              https://raw.githubusercontent.com/walter-rothlin/Source-Code/master/Datenbanken/Scripts/Create_Bankkonto.sql
+#
+# Autor: Walter Rothlin
+#
+# History:
+# 20-Oct-2021   Walter Rothlin      Initial Version
+# ------------------------------------------------------------------
+from waltisLibrary import *
+import mysql.connector
+
+
+def showKontoUebersicht(connection, cursor, title="", titleLevel=1):
+
+    if titleLevel == 1:
+        uChar = "="
+        abstand = "\n"
+    elif titleLevel == 2:
+        uChar = "-"
+        abstand = ""
+
+    print(unterstreichen(title, uChar), abstand)
+    # zeige Konto Saldos and Bilanzsumme
+    sql_show_saldo_query = """
+    select id_bankkonto, saldo from bankkonto;
+    """
+    cursor.execute(sql_show_saldo_query)
+    myresult = cursor.fetchall()
+    print("+------+--------------+")
+    print("| Id   | Saldo        |")
+    print("+------+--------------+")
+    bilanzsumme = 0
+    for aRec in myresult:
+        bilanzsumme += aRec[1]
+        print("| {id:4d} |".format(id=aRec[0]), end="")
+        print(" {saldo:12.2f} |".format(saldo=aRec[1]), end="")
+        print()
+    print("+------+--------------+")
+    print("| Summe: {summe:12.2f} |".format(summe=bilanzsumme))
+    print("+------+--------------+")
+    print("Records found:", len(myresult), myresult)
+
+
+def doKontoUebertrag(connection, cursor, withdrawAmount, fromKontoId, toKontoId, trace=True):
+    if trace:
+        showKontoUebersicht(connection, cursor, "Bevor Kontoübertrag")
+
+    # withdraw from Lohnkonto
+    sql_update_query = """Update bankkonto set saldo = saldo - """ + str(withdrawAmount) + """ where id_bankkonto = """ + str(fromKontoId)
+    cursor.execute(sql_update_query)
+
+    if trace:
+        showKontoUebersicht(connection, cursor, "Abgebucht aber noch nicht eingebucht", 2)
+        halt("Check with workebnch!! Than press RETURN to continue!")
+
+    # deposit to Sparkonto
+    sql_update_query = """Update bankkonto set saldo = saldo + """ + str(withdrawAmount) + """ where id_bankkonto = """ + str(toKontoId)
+    cursor.execute(sql_update_query)
+
+    if trace:
+        showKontoUebersicht(connection, cursor, "Eingebucht aber noch nicht commited", 2)
+        halt("Check with workebnch!! Than press RETURN to continue!")
+
+    # Commit your changes
+    connection.commit()
+    if trace:
+        showKontoUebersicht(connection, cursor, "Nach commited", 2)
+        halt("Check with workebnch!! Than press RETURN to continue!")
+
+
+dbServer = "localhost"
+dbSchema = "MyBank"
+userName = "root"
+password = "admin"
+
+try:
+    print(f"Connecting to '{dbSchema:s}' with user '{userName:s}'....", end="", flush=True)
+    # https://dev.mysql.com/doc/connector-python/en/connector-python-connectargs.html
+    conn = mysql.connector.connect(
+        host=dbServer,
+        database=dbSchema,
+        user=userName,
+        passwd=password
+    )
+    print("completed!")
+
+    conn.autocommit = False   # explicit commit and rollback by the application (Front-End)
+    cursor = conn.cursor()
+
+    showKontoUebersicht(conn, cursor, "Vor Kontoübertrag")
+    fromKonto = readInt("Belastungskonto (From): ", min=1, max=2)
+    toKonto   = readInt("Gutschriftskonto (To) : ", min=1, max=2)
+    withdrawAmount = readFloat("Uebertrags-Betrag: ", min=10, max=10000)
+
+    doKontoUebertrag(conn, cursor, withdrawAmount, fromKontoId=fromKonto, toKontoId=toKonto, trace=False)
+
+    showKontoUebersicht(conn, cursor, "Nach Kontoübertrag")
+
+
+except mysql.connector.Error as error:
+    print("Failed to update record to database rollback: {}".format(error))
+    # reverting changes because of exception
+    conn.rollback()
+finally:
+    # closing database connection.
+    if conn.is_connected():
+        cursor.close()
+        conn.close()
+        print("Connection is closed")
---
+++ Python_WaltisExamples/Code_08_MySql/05_BankenAppl_OO.py
@@ -0,0 +1,142 @@
+#!/usr/bin/python3
+
+# ------------------------------------------------------------------
+# Name: 05_BankenAppl_OO.py
+#
+# Description: Implements Kontoübertrag (OO-Lösung)
+#
+#              Use the following sql-script to create MyBank schema:
+#              https://raw.githubusercontent.com/walter-rothlin/Source-Code/master/Datenbanken/Scripts/Create_Bankkonto.sql
+#
+# Autor: Walter Rothlin
+#
+# History:
+# 20-Oct-2021   Walter Rothlin      Initial Version
+# ------------------------------------------------------------------
+from waltisLibrary import *
+import mysql.connector
+
+class Konto:
+    def __init__(self, id, saldo, limite=0):
+        self.__id = id
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def __str__(self):
+        return "id: {id:6d}    saldo: {saldo:10.2f}    limite: {limite:10.2f}".format(id=self.__id, saldo=self.__saldo, limite=self.__limite)
+
+    def toString(self, part="main"):
+        retStr = ""
+        if part == "title":
+            retStr += "+--------+------------+------------+\n"
+            retStr += "| {id:6s} | {saldo:10s} | {limite:10s} |\n".format(id="ID", saldo="Saldo", limite="Limite")
+            retStr += "+--------+------------+------------+\n"
+        if part == "main":
+            retStr += "| {id:6d} | {saldo:10.2f} | {limite:10.2f} |\n".format(id=self.__id, saldo=self.__saldo, limite=self.__limite)
+        if part == "footer":
+            retStr += "+--------+------------+------------+\n"
+            retStr += "| Summe: {summe:12.2f} |            |\n".format(summe=55.2)
+            retStr += "+--------+------------+------------+\n"
+        return retStr
+
+    def setSaldo(self, newSaldo):
+        self.__saldo = newSaldo
+
+    def getSaldo(self):
+        return self.__saldo
+
+    def getId(self):
+        return self.__id
+
+    # returns effektiver Bezug
+    def bezug(self, amount):
+        if self.__saldo - amount >= self.__limite:
+            self.__saldo -= amount
+            return amount
+        else:
+            alterSaldo = self.__saldo
+            self.__saldo = self.__limite
+            return alterSaldo - self.__limite
+
+    # returns neuer Kontostand
+    def deposit(self, amount):
+        self.__saldo += amount
+        return self.__saldo
+
+def Test_Konto():
+    print("\n")
+    print(unterstreichen("Test-Konto"))
+    sparkonto = Konto(1, 2000, 100)
+    print(sparkonto, "\n")
+    print("Bezug:   100:", sparkonto.bezug(100))
+    print(sparkonto, "\n")
+    print("Bezug:  2000:", sparkonto.bezug(2000))
+    print(sparkonto, "\n")
+    print("Einlage: 550:", sparkonto.deposit(550))
+    print(sparkonto, "\n")
+
+if __name__ == '__main__':
+    if False:
+        Test_Konto()
+
+
+# ----------------------------------------------------------------------
+class Kontoliste:
+    def __init__(self, owner="Unknown"):
+        self.__owner = owner
+        self.__kontoList = {}
+
+    def __str__(self):
+        retStr = "owner: " + str(self.__owner) + "\n"
+        for aKontoKey in self.__kontoList:
+            retStr += "    " + str(self.__kontoList[aKontoKey]) + "\n"
+        return retStr
+
+    def addKonto(self, newKonto):
+        self.__kontoList[newKonto.getId()] = newKonto
+
+    def getKontoViaID(self, id):
+        return self.__kontoList[id]
+
+    def showKontoUebersicht(self):
+        print(unterstreichen("Kontoübersicht von " + str(self.__owner)))
+        firstTime = True
+        aKonto = None
+        for aKontoKey in self.__kontoList:
+            aKonto = self.__kontoList[aKontoKey]
+            if firstTime:
+                firstTime = False
+                print(aKonto.toString("title"), end="")
+
+            print(aKonto.toString(), end="")
+        print(aKonto.toString("footer"), end="")
+
+
+    def doKontoUebertrag(self, withdrawAmount, fromKontoId, toKontoId, trace=True):
+        self.getKontoViaID(fromKontoId).bezug(withdrawAmount)
+        self.getKontoViaID(toKontoId).deposit(withdrawAmount)
+
+
+def Test_Kontoliste():
+    print("\n")
+    print(unterstreichen("Test-Kontoliste"))
+    waltisKonties = Kontoliste("Walter Rothlin")
+    waltisKonties.addKonto(Konto(1, 10000))
+    waltisKonties.addKonto(Konto(2, 2000, limite=-100))
+    # print(waltisKonties)
+    waltisKonties.showKontoUebersicht()
+    print("\n--> transfer from 1 to 2: 1111")
+    waltisKonties.doKontoUebertrag(1111, 1, 2)
+    waltisKonties.showKontoUebersicht()
+    print("\n--> transfer from 2 to 1: 4000")
+    waltisKonties.doKontoUebertrag(4000, 2, 1)
+    waltisKonties.showKontoUebersicht()
+
+
+if __name__ == '__main__':
+    if True:
+        Test_Kontoliste()
+
+# ----------------------------------------------------------------------
+# Main
+# ----------------------------------------------------------------------
