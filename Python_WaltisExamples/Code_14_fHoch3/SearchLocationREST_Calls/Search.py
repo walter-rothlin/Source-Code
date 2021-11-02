29c041ea5523a30ec8b26d2232575a761ddcdf93
walter-rothlin <walter@rothlin.com>
2021-11-02 11:12:17 +0100

    update

--- Python_WaltisExamples/Code_14_fHoch3/SearchLocationREST_Calls/Search.py
+++ Python_WaltisExamples/Code_14_fHoch3/SearchLocationREST_Calls/Search.py
@@ -60,4 +60,13 @@ for aEntry in value:
     print()
 
 print("-----------------------------------------------------------------")
+
 # Google API:    https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Peterliwiese+33&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyDAKgrjrKNmBPu47TUFP-x8hY_jp2Ainbk
+searchCriteria = "Peterliwiese+33"
+appId = "AIzaSyDAKgrjrKNmBPu47TUFP-x8hY_jp2Ainbk"
+serviceURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={search:2s}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key={appId:2s}"
+requestStr = serviceURL.format(search=searchCriteria, appId=appId)
+responseStr = requests.get(requestStr)
+jsonResponse = json.loads(responseStr.text)
+print("Request:\n", requestStr)
+print("Response:\n", jsonResponse, "\n")
