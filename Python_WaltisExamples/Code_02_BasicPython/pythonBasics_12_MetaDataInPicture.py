25b7a8cc8791eeb5619c220fd2d3e9a584421de2
walter-rothlin <walter@rothlin.com>
2022-01-28 07:57:59 +0100

    Update

--- Python_WaltisExamples/Code_02_BasicPython/pythonBasics_12_MetaDataInPicture.py
+++ Python_WaltisExamples/Code_02_BasicPython/pythonBasics_12_MetaDataInPicture.py
@@ -1,5 +1,20 @@
+#!/usr/bin/python3
+
+# ------------------------------------------------------------------
+# Name  : pythonBasics_12_MetaDataInPicture.py
+# Source: https://raw.githubusercontent.com/walter-rothlin/Source-Code/master/Python_WaltisExamples/Code_02_BasicPython/pythonBasics_12_MetaDataInPicture.py
+#
+# Description: Lädt ein Bild und liesst alle Meta-Tags
+#
+# Autor: Walter Rothlin
+#
 # https://pillow.readthedocs.io/en/stable/reference/Image.html#examples    Bildverarbeitung
 # http://metapicz.com/#landing  Web-Applikation zum Meta-Tags auslesen
+#
+# History:
+# 22.4.21   Walter Rothlin      Initial Version
+# ------------------------------------------------------------------
+
 
 from PIL import Image
 
