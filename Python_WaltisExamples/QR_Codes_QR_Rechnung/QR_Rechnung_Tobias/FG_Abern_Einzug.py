8d8cc82ce70380e38833403c7c24aaaeeac66352
Walter Rothlin <46260915+walter-rothlin@users.noreply.github.com>
2022-06-19 12:20:01 +0200

    Update

--- Python_WaltisExamples/QR_Codes_QR_Rechnung/QR_Rechnung_Tobias/FG_Abern_Einzug.py
+++ Python_WaltisExamples/QR_Codes_QR_Rechnung/QR_Rechnung_Tobias/FG_Abern_Einzug.py
@@ -17,6 +17,12 @@ from waltisLibrary import *
 
 
 if __name__ == '__main__':
+    jahr = "2022"
+    baseDestDir = 'N:\\06_FG_Abern\\' + jahr + '\\PerimeterRechnungen\\'
+    baseSourceDir = 'N:\\06_FG_Abern\\'
+
+    sourceDate = 'FG_Aberen_Kassier.xlsx'
+
     addr_FG_Aberen = {
         "name": "Flurgenossenschaft",
         "address": "'Aberen'",
@@ -29,53 +35,99 @@ if __name__ == '__main__':
         "FG_Abern": "CH90 0077 7003 2922 1166 7"
     }
 
-
-
-    # Load in the workbook
-    wb = load_workbook('N:\\06_FG_Abern\\FG_Aberen_Kassier.xlsx')
-    print(wb.sheetnames)
+    # Load in the workbook (data_only=True returns values not formulas)
+    wb = load_workbook(baseSourceDir + sourceDate,  data_only=True)
+    print("Sheets in ", sourceDate, ":", wb.sheetnames, "\n\n")
     dataSheet = wb['Perimeter']
+
+    # Strassenbenützung
+    addr_strassenbenutzung = {
+        "name": dataSheet['B17'].value + " " + dataSheet['C17'].value,
+        "address": dataSheet['D17'].value,
+        "zip_code": str(dataSheet['E17'].value),
+        "city": dataSheet['F17'].value,
+        "country": "",
+    }
+
+    pdfName = baseDestDir + "FG_Aberen_Strassenbenutzung 1"
+    htmlName = baseDestDir + "FG_Aberen_Strassenbenutzung 1" + ".html"
+
+    createQRInvoice(
+        generateQRInvoiceData(
+            ibanNr["FG_Abern"],
+            addr_FG_Aberen,
+            addr_strassenbenutzung,
+            amount="50.00",
+            additional_information="Strassenbeutzung 2022"),
+        invoice_text_html="",
+        pdfName=pdfName,
+        htmlName=htmlName)
+
+    print("Produced following files: ", pdfName)
+
 
     doLoop = True
     row = 2
     while doLoop:
-        aVal = dataSheet['A' + str(row)].value
-        print(dataSheet['L' + str(row)].value)
-        if aVal is not None:
+        anschrift = dataSheet['A' + str(row)].value
+        # print("==> ", anschrift, ";", type(anschrift), end="\n")
+
+        vorname   = dataSheet['B' + str(row)].value
+        if vorname is None:
+            vorname = ""
+        # print(";", vorname, ";", type(vorname), end="\n")
+
+        name      = dataSheet['C' + str(row)].value
+        # print(";", name, ";", type(name), end="\n")
+
+        strasse   = dataSheet['D' + str(row)].value
+        if strasse is None:
+            strasse = ""
+        # print(";", strasse, ";", type(strasse), end="\n")
+
+        plz       = str(dataSheet['E' + str(row)].value)
+        # print(";", plz, ";", type(plz), end="\n")
+
+        ort       = dataSheet['F' + str(row)].value
+        # print(";", ort, ";", type(ort), end="\n")
+
+        betrag    = dataSheet['N' + str(row)].value
+        # print(";", betrag, ";", type(betrag))
+
+
+        if anschrift is not None:
+            initialen = ""
+            if len(vorname) > 0:
+                initialen = vorname[0]
+
+            if len(anschrift) <= 4:
+                anschrift += " " + initialen + "." + name
+            # else:
+            #    anschrift += " c/o " + initialen + "." + name
+
             addr_kreditor = {
-                "name": aVal + " " + dataSheet['B' + str(row)].value,
-                "address": dataSheet['C' + str(row)].value,
-                "zip_code": "",
-                "city": dataSheet['D' + str(row)].value,
+                "name": anschrift,
+                "address": strasse,
+                "zip_code": plz,
+                "city": ort,
                 "country": "",
             }
-            print(aVal)
-            pdfName = "GeneratedInvoices/FG_Aberen_" + str(row)
-            htmlName = "GeneratedInvoices/FG_Aberen" + str(row) + ".html"
+            pdfName = baseDestDir + "FG_Aberen_" + str(row)
+            htmlName = baseDestDir + "FG_Aberen" + str(row) + ".html"
+            print(pdfName + ":    " + anschrift)
             createQRInvoice(
                 generateQRInvoiceData(
                     ibanNr["FG_Abern"],
                     addr_FG_Aberen,
                     addr_kreditor,
-                    amount="10.00",     # dataSheet['L' + str(row)].value,
-                    additional_information="FG Abern: "),
+                    amount=betrag,
+                    additional_information=jahr),
                 invoice_text_html="",
                 pdfName=pdfName,
                 htmlName=htmlName)
-
-            print("Produced following files:")
-            print("    --> ", pdfName)
-            print("    --> ", htmlName)
-
             row += 1
         else:
             doLoop = False
 
-
-
-
-
-
-
-
-
+        if (os.path.exists(htmlName)):
+            os.remove(htmlName)
