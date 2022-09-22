66cf83ed38f21164142e11738f883c9904d4f369
Walter Rothlin <46260915+walter-rothlin@users.noreply.github.com>
2022-09-22 19:55:17 +0200

    Update

--- Python_WaltisExamples/Code_13_TcpIP/TCP_Server_0.py
+++ Python_WaltisExamples/Code_13_TcpIP/TCP_Server_0.py
@@ -17,7 +17,7 @@ import socket
 # https://realpython.com/python-sockets/
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+PORT = 65433        # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
