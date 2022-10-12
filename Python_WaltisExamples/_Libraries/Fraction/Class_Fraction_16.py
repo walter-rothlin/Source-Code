ac8892ed14e79f3d0156596bb942367dc069e870
Walter Rothlin <46260915+walter-rothlin@users.noreply.github.com>
2022-10-12 08:04:23 +0200

    Update Class_Fraction_16.py

--- Python_WaltisExamples/_Libraries/Fraction/Class_Fraction_16.py
+++ Python_WaltisExamples/_Libraries/Fraction/Class_Fraction_16.py
@@ -11,7 +11,7 @@
 #
 # History:
 # 11-Oct-2022	Walter Rothlin Initial Version __int__ __str__ and main
-# 12-Oct-2022	Walter Rothlin Kehrwert und automated Testing
+# 12-Oct-2022	Walter Rothlin Kehrwert und automated testing
 #
 # ------------------------------------------------------------------
 
@@ -54,7 +54,7 @@ class Fraction:
     nenner = property(get_nenner, set_nenner)
 
     # Business Methods
-    def reciprocal(self):   # only Prototype
+    def reciprocal(self):
         """
         - setzt self moit dem Kehrwert [1/2] ==> [2/1]
         """
@@ -63,39 +63,112 @@ class Fraction:
         self.__nenner = tmp
         return self
 
+
+def TEST_init_str(verbal=False):
+    error_count = 0
+    test_count = 0
+
+    bruch = Fraction()
+    expected = "[0/1]"
+    test_count += 1
+    if str(bruch) != expected:
+        print("1." + str(test_count) + ") ERROR:: Expected: " + expected + "    Actual:", bruch)
+        error_count += 1
+
+    bruch = Fraction(5)
+    expected = "[5/1]"
+    test_count += 1
+    if str(bruch) != expected:
+        print("1." + str(test_count) + ") ERROR:: Expected: " + expected + "    Actual:", bruch)
+        error_count += 1
+
+    bruch = Fraction(3, 4)
+    expected = "[3/4]"
+    test_count += 1
+    if str(bruch) != expected:
+        print("1." + str(test_count) + ") ERROR:: Expected: " + expected + "    Actual:", bruch)
+        error_count += 1
+
+    bruch = Fraction(zaehler=3)
+    expected = "[3/1]"
+    test_count += 1
+    if str(bruch) != expected:
+        print("1." + str(test_count) + ") ERROR:: Expected: " + expected + "    Actual:", bruch)
+        error_count += 1
+
+    bruch = Fraction(zaehler=5, nenner=2)
+    expected = "[5/2]"
+    test_count += 1
+    if str(bruch) != expected:
+        print("1." + str(test_count) + ") ERROR:: Expected: " + expected + "    Actual:", bruch)
+        error_count += 1
+
+    if verbal:
+        print("1) Test __init__ and __str__")
+        print("----------------------------")
+        print("     Test performed: ", test_count)
+        print("     Test failed   : ", error_count)
+        print("\n")
+
+def TEST_setter_getter_properties(verbal=False):
+    error_count = 0
+    test_count = 0
+
+    bruch = Fraction()
+    bruch.set_zaehler(1)
+    bruch.set_nenner(2)
+    expected = "[1/2]"
+    test_count += 1
+    if str(bruch) != expected:
+        print("2." + str(test_count) + ") ERROR:: Expected: " + expected + "    Actual:", bruch)
+        error_count += 1
+
+    bruch = Fraction()
+    bruch.zaehler = 7
+    bruch.nenner = 8
+    expected = "[7/8]"
+    test_count += 1
+    if str(bruch) != expected:
+        print("2." + str(test_count) + ") ERROR:: Expected: " + expected + "    Actual:", bruch)
+        error_count += 1
+
+    if verbal:
+        print("2) Test Setter / Getter and Properties")
+        print("--------------------------------------")
+        print("     Test performed: ", test_count)
+        print("     Test failed   : ", error_count)
+        print("\n")
+
+def TEST_reciprocal(verbal=False):
+    error_count = 0
+    test_count = 0
+
+    bruch = Fraction(7, 8)
+    bruch.reciprocal()
+    expected = "[8/7]"
+    test_count += 1
+    if str(bruch) != expected:
+        print("3." + str(test_count) + ") ERROR:: Expected: " + expected + "    Actual:", bruch)
+        error_count += 1
+
+    bruch = Fraction(3, 4)
+    bruch.reciprocal()
+    expected = "[4/3]"
+    test_count += 1
+    if str(bruch) != expected:
+        print("3." + str(test_count) + ") ERROR:: Expected: " + expected + "    Actual:", bruch)
+        error_count += 1
+
+    if verbal:
+        print("3) Test reciprocal methode")
+        print("--------------------------")
+        print("     Test performed: ", test_count)
+        print("     Test failed   : ", error_count)
+        print("\n")
+
+
 if __name__ == '__main__':
+    TEST_init_str(verbal=True)
+    TEST_setter_getter_properties(verbal=True)
+    TEST_reciprocal(verbal=True)
 
-    print("1) Test __init__ and __str__")
-    print("----------------------------")
-    bruch_1 = Fraction()
-    print("1.1) bruch_1: [0/1]::", bruch_1)
-
-    bruch_2 = Fraction(5)
-    print("1.2) bruch_2: [5/1]::", bruch_2)
-
-    bruch_3 = Fraction(3, 4)
-    print("1.3) bruch_3: [3/4]::", bruch_3)
-
-    bruch_4 = Fraction(zaehler=3)
-    print("1.4) bruch_4: [3/1]::", bruch_4)
-
-    bruch_5 = Fraction(zaehler=5, nenner=2)
-    print("1.5) bruch_5: [5/2]::", bruch_5)
-
-    print("\n")
-
-    print("2) Test Setter / Getter and Properties")
-    print("--------------------------------------")
-    bruch_1.set_zaehler(1)
-    bruch_1.set_nenner(2)
-    print("2.1) bruch_1: [1/2]::", bruch_1)
-
-    bruch_1.zaehler = 7
-    bruch_1.nenner = 8
-    print("2.2) bruch_1: [7/8]::", bruch_1)
-
-
-    print("3) Test reciprocal methode")
-    print("--------------------------")
-    print("3.1) bruch_1: [8/7]::", bruch_1.reciprocal())
-    print("\n")
