5b41e6704bc3cdcb949f3fb85058360a4717bc72
Walter Rothlin <46260915+walter-rothlin@users.noreply.github.com>
2022-11-11 07:56:21 +0100

    Update

---
+++ Python_WaltisExamples/_BZU/2022/Verschluesser_B.py
@@ -0,0 +1,21 @@
+# ---------------------------------------
+# Walter Rothlin
+# ---------------------------------------
+
+print("Hello", 'BZU', "!!!!??????")
+
+eineZahl = 123.45    # float (rationale Zahlen)
+anzahl = 12          # int (Ganze Zahl)
+wasBinIch = 12.0     # float
+
+vorname = "Walti"
+nachname = "Rothlin"
+
+print(vorname + ' ' + nachname)
+
+print(eineZahl, eineZahl * 10)
+
+buchstabe = 'A'
+
+print(buchstabe, ord(buchstabe), hex(ord(buchstabe)), oct(ord(buchstabe)), bin(ord(buchstabe)))
+
---
+++ Python_WaltisExamples/_BZU/2022/Verschluessler_A.py
@@ -0,0 +1,42 @@
+# ------------------------------
+# Walter Rothlin
+# ------------------------------
+
+
+print("Verschluessler")
+print("--------------")
+
+eineZahl_0 = 1234     # Kommentar
+_eineZahl_0 = 234.56
+__eineZahl_0 = 1.0
+
+print(eineZahl_0, _eineZahl_0, __eineZahl_0, eineZahl_0 + __eineZahl_0)
+
+vorname = "Walti"
+nachname = 'Haemmerli'
+print(vorname, nachname, eineZahl_0)
+
+print(nachname + " " + vorname)
+
+
+print("A ==> ", ord("A"))
+print("65 ==> ", chr(65))
+print(chr(ord("R") + 32))
+
+eineZahl = 65
+print(eineZahl, hex(eineZahl), bin(eineZahl), oct(eineZahl), "==>", chr(eineZahl))
+
+klartext = input("Klartext:")
+shifter = int(input("Shifter:"))
+chiffrat = ""
+for einBuchstabe in klartext:
+    print(einBuchstabe, chr(ord(einBuchstabe) + shifter))
+    chiffrat = chiffrat + chr(ord(einBuchstabe) + shifter)
+print(klartext, "==>", chiffrat)
+
+'''
+0   00000000   0o00000     0x00
+
+
+'''
+
