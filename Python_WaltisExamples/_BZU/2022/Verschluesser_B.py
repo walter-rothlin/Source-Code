fd4d5193921b80dad9b29a79b35858bfc12d4fac
Walter Rothlin <46260915+walter-rothlin@users.noreply.github.com>
2022-11-14 14:30:01 +0100

    Update

--- Python_WaltisExamples/_BZU/2022/Verschluesser_B.py
+++ Python_WaltisExamples/_BZU/2022/Verschluesser_B.py
@@ -2,6 +2,9 @@
 # Walter Rothlin
 # ---------------------------------------
 
+def ascii_shifter(buchstabeP, shifterP):
+    return chr(ord(buchstabeP) + shifterP)
+
 print("Hello", 'BZU', "!!!!??????")
 
 eineZahl = 123.45    # float (rationale Zahlen)
@@ -16,6 +19,27 @@ print(vorname + ' ' + nachname)
 print(eineZahl, eineZahl * 10)
 
 buchstabe = 'A'
+shifter = 5
 
 print(buchstabe, ord(buchstabe), hex(ord(buchstabe)), oct(ord(buchstabe)), bin(ord(buchstabe)))
 
+
+print(buchstabe, "(", shifter, ")   ==>", chr(ord(buchstabe) + shifter))
+
+buchstabe = '*'
+shifter = 9
+print(buchstabe, "(", shifter, ")   ==>", chr(ord(buchstabe) + shifter))
+
+
+print(buchstabe, "(", shifter, ") ==> ", ascii_shifter(buchstabe, shifter))
+print('A', "(", 5, ") ==> ", ascii_shifter('A', 5))
+
+test1_chr = "3"
+test1_shifter = 4
+test1_expected = "7"
+print("ascii_shifter(" + test1_chr + ", " + str(test1_shifter) + ") = ", ascii_shifter(test1_chr, test1_shifter), "   Expected: " + test1_expected)
+
+test1_chr = "A"
+test1_shifter = 5
+test1_expected = "F"
+print("ascii_shifter(" + test1_chr + ", " + str(test1_shifter) + ") = ", ascii_shifter(test1_chr, test1_shifter), "   Expected: " + test1_expected)
