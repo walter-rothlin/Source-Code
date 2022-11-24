d5ba034987acfb832863b7a5be3bb352deb72204
Walter Rothlin <46260915+walter-rothlin@users.noreply.github.com>
2022-11-24 15:19:37 +0100

    Update

---
+++ Python_WaltisExamples/_HWZ/BWI_A21/LN/Abgaben/Fabian_Ammann_BWI_A21_DS.py
@@ -0,0 +1,264 @@
+import os
+
+import requests
+import json
+import time
+import datetime
+
+
+def get_formatted_timestamp():
+    return datetime.datetime.now().isoformat(sep=" ", timespec="seconds")
+
+
+class Logger:
+    """
+    This is a reusable Logger Class with an easy interface
+    Following Properties can be set via setter or initializer overloading:
+    delimiter (Default: "|")
+    file_to_write_path (Default: "./log.csv)
+    scroll_after (Default: 0 -> No scrolling)
+    only_changes (Default: True)
+    append_or_new (Default: append)
+    """
+
+    def __init__(self, delimiter=';', append_or_new='a', only_changes=True, scroll_after=0,
+                 file_to_write_path='.//log.csv'):
+        self.__delimiter = delimiter
+        self.__only_changes = only_changes
+        self.__append_or_new = append_or_new
+        self.__file_to_write_path = file_to_write_path
+        self.__scroll_after = scroll_after
+
+    def set_file_to_write_path(self, file_to_write_path):
+        self.__file_to_write_path = file_to_write_path
+
+    def set_delimiter(self, delimiter):
+        self.__delimiter = delimiter
+
+    def set_append_or_new(self, append_or_new):
+        self.__append_or_new = append_or_new
+
+    def set_scroll_after(self, scroll_after):
+        self.__scroll_after = scroll_after
+
+    def set_only_changes(self, only_changes):
+        self.__only_changes = only_changes
+
+    def log_to_file(self, log_lvl, log_string):
+        """
+        This is the main Logger function
+        It logs new content to the file, defined in the logger settings
+        It has multiple logging strategies, f.e. scrolling or onlyChanges
+        """
+        string_to_write = log_lvl + self.__delimiter + log_string
+
+        # Scrolling functionality
+        if self.__scroll_after > 0:
+            with open(self.__file_to_write_path, "r") as file:
+                lines = file.readlines()
+            if len(lines) > self.__scroll_after:
+                with open(self.__file_to_write_path, "w") as file:
+                    # Write Header
+                    file.write(lines[0])
+                    file.write(lines[1])
+                    # Write Lines
+                    counter = len(lines) - (self.__scroll_after - 1)
+                    while counter < len(lines):
+                        file.write(lines[counter])
+                        counter = counter + 1
+
+        # Only Changes functionality
+        if self.__only_changes:
+            with open(self.__file_to_write_path) as f:
+                for line in f:
+                    pass
+                if self.__delimiter in line:
+                    last_line = line.split(self.__delimiter, 1)[1].replace('\n', '')
+                    if last_line == string_to_write:
+                        return
+        file_to_write = open(self.__file_to_write_path, self.__append_or_new)
+        file_to_write.write(get_formatted_timestamp() + self.__delimiter + string_to_write + '\n')
+
+    def log_header(self, header_to_log):
+        file_to_write = open(self.__file_to_write_path, 'a')
+        file_to_write.write(header_to_log + '\n')
+
+    def prepare_file(self):
+        if not os.path.isfile(self.__file_to_write_path):
+            open(self.__file_to_write_path, 'x')
+
+    def delete_content(self):
+        open(self.__file_to_write_path, 'w').close()
+
+    def header_exists(self):
+        print(os.path.isfile(self.__file_to_write_path))
+        if not os.path.isfile(self.__file_to_write_path):
+            open(self.__file_to_write_path, 'x')
+            return False
+        print(os.stat(self.__file_to_write_path).st_size == 0)
+        if os.stat(self.__file_to_write_path).st_size == 0:
+            return False
+        else:
+            return True
+
+    def log_string_builder(self, value_list):
+        value_string = ''
+        for value in value_list:
+            value_string += str(value)
+            value_string += self.__delimiter
+
+        return value_string
+
+    def get_header_prefix(self):
+        return 'timestamp' + self.__delimiter + 'Log Level' + self.__delimiter
+
+    def get_header_title(self):
+        return '# <Filename>' + self.__file_to_write_path + '</Filename>' + \
+               '<Date>' + get_formatted_timestamp() + '</Date> '
+
+    def debug(self, value_list):
+        self.log_to_file('debug', value_list)
+
+    def info(self, value_list):
+        self.log_to_file('info', value_list)
+
+    def warning(self, value_list):
+        self.log_to_file('warning', value_list)
+
+    def error(self, value_list):
+        self.log_to_file('error', value_list)
+
+    def critical(self, value_list):
+        self.log_to_file('critical', value_list)
+
+
+class WeatherResponseDomain:
+    """
+    This is the WeatherResponseDomain Object
+    It is a simple Object with the information and basic functionalities of the weather request information
+    """
+    def __init__(self, json_response):
+        self.placeName = json_response['name']
+        self.land = json_response['sys']['country']
+        self.temp = json_response['main']['temp']
+        self.pressure = json_response['main']['pressure']
+        self.humidity = json_response['main']['humidity']
+        self.lon = json_response['coord']['lon']
+        self.lat = json_response['coord']['lat']
+        self.cloud = json_response['weather'][0]['description']
+        self.windSpeed = json_response['wind']['speed']
+        self.windDirection = json_response['wind']['deg']
+
+    def get_values_as_list(self):
+        return [self.placeName, self.land, self.temp, self.pressure, self.humidity, self.lon, self.lat,
+                self.cloud, self.windSpeed, self.windDirection]
+
+    @staticmethod
+    def get_header_as_list():
+        return ['placeName', 'land', 'temp', 'pressure', 'humidity', 'lon', 'lat',
+                'cloud', 'windSpeed', 'windDirection']
+
+
+class WeatherApiService:
+    """
+    This is the WeatherApiService
+    Its a service which does simple request to the weather api
+    """
+    # Config of WeatherService
+    api_url = "https://api.openweathermap.org/data/2.5/weather"
+    place_prefix = "?q="
+    path_variables = "&units=metric&lang=de&appid="
+    app_id = "44ab77490cd2f2ca7954986aaad2e13c"
+
+    @staticmethod
+    def get_url(reqeust_place):
+        return WeatherApiService.api_url + \
+               WeatherApiService.place_prefix + \
+               reqeust_place + \
+               WeatherApiService.path_variables + \
+               WeatherApiService.app_id
+
+    @staticmethod
+    def get_weather_data_of_place(request_url):
+        return requests.get(request_url)
+
+
+if __name__ == '__main__':
+    """
+    This is the main program. It brings everything together
+    """
+    # User Inputs
+    user_number_requests = int(input('How many API Request should be done (number):\t\t\t\t\t\t\t\t'))
+    user_polling_time = int(input('How many seconds between the requests (number):\t\t\t\t\t\t\t\t'))
+    user_place = input('For which place do you want todo the api call (string):\t\t\t\t\t\t')
+
+    if user_place == '':
+        user_place = 'Uster'
+        print('Taking default place -> Uster')
+
+    user_delimiter = input('Which delimiter do you want (string):\t\t\t\t\t\t\t\t\t\t')
+    if user_delimiter == '':
+        user_delimiter = '|'
+        print('Taking default delimiter -> |')
+
+    user_file_output = input('In which File should be logged (string):\t\t\t\t\t\t\t\t\t')
+    if user_file_output == '':
+        user_file_output = './/log.csv'
+        print('Taking default File  -> .//log.csv')
+
+    user_append_or_new = input('Press x to create a new File or a for appending to an existing (x or a):\t')
+    if user_append_or_new == '':
+        user_append_or_new = 'a'
+        print('Taking default -> a')
+
+    user_only_changes = input('Type y if only changes should be logged to the logging file (y or n):\t\t')
+    if user_only_changes == '':
+        user_only_changes = 'n'
+        print('Taking default -> n')
+
+    if user_only_changes == 'y':
+        user_only_changes = True
+    else:
+        user_only_changes = False
+
+    user_scroll_after = int(input('After how many lines should be scrolled, 0 for no scrolling (number):\t\t'))
+
+    # New Logger with User or Default Settings
+    logger = Logger(user_delimiter, user_append_or_new, user_only_changes, user_scroll_after, user_file_output)
+
+    # Create Request URL
+    url = WeatherApiService.get_url(user_place)
+
+    # Prepare File
+    if user_append_or_new == 'x':
+        logger.delete_content()
+
+    # Create Header if not exists for every case
+    if not logger.header_exists():
+        title = logger.get_header_title()
+        header = logger.get_header_prefix() + logger.log_string_builder(WeatherResponseDomain.get_header_as_list())
+        # write Header to log
+        logger.log_header(title)
+        logger.log_header(header)
+        # Console output for Header
+        print('Header Printed')
+
+    # Set append mode
+    logger.set_append_or_new('a')
+
+    # Do Weather Api Requests and Log to File
+    for x in range(0, user_number_requests):
+        # Get Weather Data
+        response = WeatherApiService.get_weather_data_of_place(url)
+        # Load Content of Response
+        json_string = json.loads(response.text)
+        # Create Weather Domain Object with Content of Response
+        weather_domain = WeatherResponseDomain(json_string)
+        # Create String for Log
+        values_with_delimiter = logger.log_string_builder(weather_domain.get_values_as_list())
+        # Log Object Content to File via Logger
+        logger.info(values_with_delimiter)
+        # Console output for the current counter
+        print('Request: ' + str(x + 1))
+        # Polling Time sleeper
+        time.sleep(user_polling_time)
---
+++ Python_WaltisExamples/_HWZ/BWI_A21/LN/Abgaben/WeatherLogger_aktuell.py
@@ -0,0 +1,194 @@
+import datetime
+import requests
+import json
+import time
+import csv
+import os.path
+from enum import Enum
+
+# Edgar Golub
+# Weatherlogger mit JSON aus Waltis WeatherLogger_01.py siehe Unterricht
+# Quellen : https://www.youtube.com/@TheMorpheusTutorials ; https://www.youtube.com/@ProgrammierenStarten ; https://www.youtube.com/watch?v=spXdDP7J0TA&list=PLIrsP8dft12CSv-KEbiXq21JmR3LUr854
+#https://www.tutorialsteacher.com/python/ \ Quellen werden auch mitten im Code erwähnt
+
+
+# Time-Stamp Format
+timestamp_format = "{ts:%Y-%m-%d %H:%M:%S}"
+# Delimiter ersatz für die Kommas
+delimiter_csv = "|"
+#True/False für die OnlyChanges  https://www.datacamp.com/tutorial/case-conversion-python
+onlychanges = input("Soll nur die Änderung geschrieben werden? JA oder NEIN")
+if (onlychanges.lower() == "ja") or (onlychanges.lower() == "j") or (onlychanges.lower() == "y"):
+    only_changes = True
+else:
+    only_changes = False
+
+# Save changes Only /  Aus Lektion Nenner / Brüche kopiert + adaptiert
+def __init__(self,changes_only=True):
+    self.only_changes = changes_only
+
+# class Enum import enum https://docs.python.org/3/library/enum.html
+class Loglevel(Enum):
+    CRITICAL = 1
+    ERROR = 2
+    WARNING = 3
+    INFO = 4
+    DEBUG = 5
+
+## Erstellung Class und Writer CSV (https://docs.python.org/3/library/csv.html)
+class Logger2:
+
+    # 1st Try
+    #LEVEL_TXT = ["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"]
+    #LEVEL = [0,1,2,3,4]
+
+    def get_timestamp(self):
+        return self.timestamp_format.format(ts=datetime.datetime.now())
+
+    # __init__ Aus Lektion Nenner / Brüche kopiert + adaptiert
+    def __init__(self, file="weather_logs.csv", path=".", delimiter="|", overwrite_file=False, timestamp="{ts:%Y-%m-%d %H:%M:%S}", log_lev=Loglevel.DEBUG):
+        self.overwrite = overwrite_file
+        self.filename = file
+        self.filepath = path
+        self.delimiter_csv = delimiter
+        self.timestamp_format = timestamp
+        self.log_level = log_lev
+
+    # Getter / Setter Methode aus Lektion 1 kopiert + adaptiert
+
+    def set_delimiter(self, delimiter):
+        self.delimiter_csv = delimiter
+
+    def set_filename(self, file):
+        self.filename = file
+
+    def set_filepath(self, path):
+        self.filepath = path
+
+    def get_file(self):
+        return self.filepath + "/" + self.filename
+    #Header erstellt für XML View CSV
+    def creator_header(self):
+        try:
+            existing_file = os.path.exists(self.get_file())
+            if self.overwrite or False is existing_file:
+                mode = 'w'
+                with open(self.get_file(), mode, newline='') as csvfile:
+                    spamwriter = csv.writer(csvfile, delimiter=self.delimiter_csv,
+                                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                    # XML Format aus Beispiel eines Log-Files
+                    comment = ["#<Name>" + self.filename + "</Name> <Date>" + self.get_timestamp() + "</Date>", ""]
+                    spamwriter.writerow(comment)
+
+                    row = ["timestamp", "level", "kommentar"]
+                    spamwriter.writerow(row)
+        except FileNotFoundError:
+            print("Die Datei " + self.get_file() + " konnte nicht gefunden werden!")
+            quit()
+    #Eintrag  im Log
+    def write_line(self, level, timestamp, text):
+
+        if level.value <= self.log_level.value:
+            with open(self.get_file(), 'a', newline='') as csvfile:
+                spamwriter = csv.writer(csvfile, delimiter=self.delimiter_csv,
+                                        quotechar='\'', quoting=csv.QUOTE_MINIMAL)
+                # Arrays erstellen für row https://www.w3schools.com/python/python_arrays.asp
+                row = [timestamp, level.name, text]
+                #   spamwriter.writerow(timestamp + delimiter_csv + level + delimiter_csv + text) in array abgefuellt
+                spamwriter.writerow(row)
+
+
+def get_timestamp():
+    return timestamp_format.format(ts=datetime.datetime.now())
+
+serviceURL = "https://api.openweathermap.org/data/2.5/weather"
+pollingTime = float(input("Polling-Time [s]:"))
+ort = input("Ort  [*Dietlikon]   :")
+if ort == "":
+    ort = 'Dietlikon'
+
+max_counter = int(input("Anzahl requests :"))
+#Eigene AppID von Weather API
+appId = "f03b0b8408c3522d6e96f8c0e3c0e36d"
+
+logger2: Logger2 = Logger2("weather_logs.csv",".", "|", False, timestamp_format, Loglevel.INFO)
+
+firstTime = True
+counter = 0
+doLoop = True
+
+logger2.creator_header()
+
+logger2.write_line(Loglevel.DEBUG, get_timestamp(), "Starting ...")
+
+vor_wetter = ()
+
+while doLoop:
+    counter += 1
+    requestStr = serviceURL + "?q=" + ort + "&units=metric&lang=de&appid=" + appId
+    print("Request:\n", requestStr, "\n\n") if firstTime else False
+
+    # text = "Calling web-service " + requestStr + " ..."
+    # logger2.write_line(Loglevel.DEBUG, get_timestamp(), text)
+
+
+    # Aufruf Daten von Server Weather
+    responseStr = requests.get(requestStr)
+
+    # Debug Response level 200
+    print("Response:\n", responseStr.text, "\n\n") if firstTime else False
+
+    jsonResponse = json.loads(responseStr.text)
+
+    code = responseStr.status_code
+    reason = responseStr.reason
+
+    ortsname = jsonResponse['name']
+    land = jsonResponse['sys']['country']
+
+    temp = jsonResponse['main']['temp']
+    pressure = jsonResponse['main']['pressure']
+    humidity = jsonResponse['main']['humidity']
+    cloud = jsonResponse['weather'][0]['description']
+
+    # lat = jsonResponse['coord']['lat']
+    # lon = jsonResponse['coord']['lon']
+    # cloud = jsonResponse['weather'][0]['description']
+    # windSpeed = jsonResponse['wind']['speed']
+    # windDirection = jsonResponse['wind']['deg']
+
+    # Array für Wetterwerte
+    wetter = [ortsname, land, cloud, temp, pressure, humidity]
+
+    # Schleife für log-level
+
+    if 200 <= code <= 299:
+        if vor_wetter != wetter or only_changes == False:
+            vor_wetter = wetter
+            # text in variable gepackt +  anpassung von ausgabe logger2.write_line
+
+            #Runden siehe Aufgabe
+            temperature = round(temp, 1)
+
+            text = land + str(delimiter_csv) + ortsname + str(delimiter_csv) + str(cloud) + str(delimiter_csv) + str(
+                temperature) + str(delimiter_csv) + str(pressure) + str(delimiter_csv) + str(humidity)
+            # Anpassung Logger mit str() Ausgabe in das .CSV + Anpassung INFO hardcoded
+            logger2.write_line(Loglevel.INFO, get_timestamp(), text)
+        else:
+            text = "Wetter hat sich nicht geaendert"
+            logger2.write_line(Loglevel.DEBUG, get_timestamp(), text)
+    elif 300 <= code <= 399:
+        logger2.write_line(Loglevel.WARNING,  get_timestamp(),
+                           "Calling web-service " + requestStr + ": " + str(code) + " " + reason)
+    elif 500 <= code:
+        logger2.write_line(Loglevel.CRITICAL,  get_timestamp(),
+                           "Calling web-service " + requestStr + ": " + str(code) + " " + reason)
+    else:
+        vor_wetter = wetter
+        text = "Calling web-service " + requestStr + ": " + str(code) + " " + reason
+        logger2.write_line(Loglevel.ERROR,  get_timestamp(), text)
+
+    time.sleep(pollingTime)
+    firstTime = False
+    if counter >= max_counter:
+        doLoop = False
