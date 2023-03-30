c1058ce0a30e6c4a567e55eb43a65c1b5f828811
Walter Rothlin <46260915+walter-rothlin@users.noreply.github.com>
2023-03-30 16:48:00 +0200

    update

--- Python_WaltisExamples/_BZU/2023/TI22_A_Umrechner.py
+++ Python_WaltisExamples/_BZU/2023/TI22_A_Umrechner.py
@@ -1,15 +1,55 @@
-menu_str = """
-  Umrechnungen
-  ============
-  1: Grad in Bogenmass
-  2: Bogenmass in Grad
+#!/usr/bin/python3
 
-  3: Fahrenheit in Celsius
-  4: Celsius in Fahrenheit
+# ------------------------------------------------------------------
+# Name  : TI22_A_Umrechner.py
+# Source: https://raw.githubusercontent.com/walter-rothlin/Source-Code/master/Python_WaltisExamples/BZU/2023/TI22_A_Umrechner.py
+#
+# Description: Rechnet verschiedene physikalische Grössen um.
+#
+# Autor: Walter Rothlin
+#
+# History:
+# 23-Mar-2023   Walter Rothlin      Initial Version
+# 30-Mar-2023   Walter Rothlin      Added Loop, if and formulas
+# ------------------------------------------------------------------
+pi = 3.1415926
 
-  0: Schluss
-"""
+do_loop = True
+while do_loop:
+    menu_str = """
+      Umrechnungen
+      ============
+      1: Grad in Bogenmass
+      2: Bogenmass in Grad
+    
+      3: Fahrenheit in Celsius
+      4: Celsius in Fahrenheit
+    
+      0: Schluss
+    """
 
-print(menu_str)
-print("5*4=", 5*4, end="\nNächste Zeile\n")
-print("Ende")
+    print('\033[2J')   # cls
+    print('\033[H')    # home
+    print(menu_str)
+    wahl = input('   Wähle:')
+    if wahl == '0':
+        do_loop = False
+    elif wahl == '1':      # rad  = grad*pi/180
+        print("Grad in RAD!!!")
+        grad_value = float(input('Grad:'))
+        rad_value = grad_value*pi/180
+        print('Grad:{grad:1.2f}    Rad:{rad:1.4f}'.format(grad=grad_value, rad=rad_value))
+
+    elif wahl == '2':      # grad = rad*180/pi
+        print("RAD in Grad!!!")
+        rad_value = float(input('Rad:'))
+        grad_value = rad_value*180/pi
+        print('Rad:{rad:1.2f}    Grad:{grad:1.4f}'.format(grad=grad_value, rad=rad_value))
+    elif wahl == '3':
+        print("Fahrenheit in Celsius!")
+    elif wahl == '4':
+        print("Celsius in Fahrenheit!")
+    else:
+        print('Ungültige Eingabe!!!!')
+
+print("Programm Ende!!!!")
---
+++ Python_WaltisExamples/_BZU/2023/TI22_B_Umrechner.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+# ------------------------------------------------------------------
+# Name  : TI22_B_Umrechner.py
+# Source: https://raw.githubusercontent.com/walter-rothlin/Source-Code/master/Python_WaltisExamples/BZU/2023/TI22_B_Umrechner.py
+#
+# Description: Rechnet verschiedene physikalische Grössen um.
+#
+# Autor: Walter Rothlin
+#
+# History:
+# 23-Mar-2023   Walter Rothlin      Initial Version
+# 30-Mar-2023   Walter Rothlin      Added Loop, if and formulas
+# ------------------------------------------------------------------
+pi = 3.1415926
+
+menu_str = """
+  Umrechnungen
+  ============
+  1: Grad in Bogenmass
+  2: Bogenmass in Grad
+
+  3: Fahrenheit in Celsius
+  4: Celsius in Fahrenheit
+
+  0: Schluss
+"""
+
+do_loop = True
+while do_loop:
+    print(menu_str)
+    wahl = input('Wähle:')
+    if wahl == '1':     # rad  = grad*pi/180
+        print('Grad to Rad')
+        grad_value = float(input('Wieviel Grad:'))
+        rad_value = grad_value * pi / 180
+        print('Grad:{gr:1.0f}   -->  Rad:{ra:1.4f}'.format(gr=grad_value, ra=rad_value))
+
+    elif wahl == '2':   # grad = rad*180/pi
+        print('Rad to Grad')
+        rad_value = float(input('Wieviel RAD:'))
+        grad_value = rad_value * 180 / pi
+        print('Rad:{ra:1.4f}   -->  Grad:{gr:1.0f}'.format(gr=grad_value, ra=rad_value))
+    elif wahl == '3':
+        print('Celsius to Fahrenheit')
+    elif wahl == '4':
+        print("Fahrenheit to Celsius")
+    elif wahl == '0':
+        do_loop = False
+        print("Beenden")
+    else:
+        print('Bitte eine gültige Eingabe machen')
+
