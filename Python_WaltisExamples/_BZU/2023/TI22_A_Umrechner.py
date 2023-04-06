9622732f0f0760799c3d7f4a5b53a7988f37ae85
Walter Rothlin <46260915+walter-rothlin@users.noreply.github.com>
2023-04-06 13:13:24 +0200

    Update TI22_A_Umrechner.py

--- Python_WaltisExamples/_BZU/2023/TI22_A_Umrechner.py
+++ Python_WaltisExamples/_BZU/2023/TI22_A_Umrechner.py
@@ -19,11 +19,11 @@ while do_loop:
     menu_str = """
       Umrechnungen
       ============
-      1: Grad in Bogenmass
-      2: Bogenmass in Grad
+      1: Grad in Bogenmass        rad  = grad*pi/180
+      2: Bogenmass in Grad        grad = rad*180/pi
     
-      3: Fahrenheit in Celsius
-      4: Celsius in Fahrenheit
+      3: Fahrenheit in Celsius    32F -> 0°C    100F -> 38.8°C     °C = (°F - 32) / 1.8
+      4: Celsius in Fahrenheit    32F -> 0°C    100F -> 38.8°C     °F = (°C * 1.8) - 32
     
       0: Schluss
     """
@@ -34,6 +34,7 @@ while do_loop:
     wahl = input('   Wähle:')
     if wahl == '0':
         do_loop = False
+
     elif wahl == '1':      # rad  = grad*pi/180
         print("Grad in RAD!!!")
         grad_value = float(input('Grad:'))
@@ -45,10 +46,19 @@ while do_loop:
         rad_value = float(input('Rad:'))
         grad_value = rad_value*180/pi
         print('Rad:{rad:1.2f}    Grad:{grad:1.4f}'.format(grad=grad_value, rad=rad_value))
+
     elif wahl == '3':
         print("Fahrenheit in Celsius!")
+        fahr_value = float(input('Fahrenheit:'))
+        cel_value = (fahr_value - 32) / 1.8
+        print('Fahrenheit:{far:1.2f}    Celsius:{cel:1.4f}'.format(far=fahr_value, cel=cel_value))
+
     elif wahl == '4':
         print("Celsius in Fahrenheit!")
+        cel_value = float(input('Celsius:'))
+        fahr_value = (cel_value*1.8) - 32
+        print('Celsius:{cel:1.2f}    Fahrenheit:{far:1.4f}'.format(cel=cel_value, far=fahr_value))
+
     else:
         print('Ungültige Eingabe!!!!')
 
