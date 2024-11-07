7979db8044b4f43c1a1974472d25ecba04895db0
Walter Rothlin <46260915+walter-rothlin@users.noreply.github.com>
2024-11-07 12:56:36 +0100

    Update

--- SchulungsUnterlagen/Studentenbeispiele/BZU/24_25/M231/Shifter.py
+++ SchulungsUnterlagen/Studentenbeispiele/BZU/24_25/M231/Shifter.py
@@ -1,2 +1,18 @@
+#
+# ================================================
+# ================================================
+
+
 print('Shifter')
 print('=======')
+
+print('Hallo' + "BZU", 123+5, '123', 3.14, True, False, 'True', sep=';;', end='--->')
+print('Ha"\'ll####o', "Ha'l\"l\\\"o", sep=' ', end='\n') # Default Values for sep and end
+print("\nHallo\nBZU")
+pi = 3.1415926
+
+print(pi)
+
+radius = float(input('Kreisradius:'))
+print(f'Radius:{radius:7.2f} --> Umfang:{2 * radius * pi:0.2f}')
+
