7f8a90ae6c07f7be377b6c6f0b02b5cf56f94a37
Walter Rothlin <46260915+walter-rothlin@users.noreply.github.com>
2024-12-01 19:31:12 +0100

    Update

---
+++ Python_Raspberry/03_PiPlates/01_PiPlates.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+# ------------------------------------------------------------------
+# Source: https://raw.githubusercontent.com/walter-rothlin/Source-Code/master/Python_Raspberry/03_PiPlates/01_PiPlates.py
+#
+# Description: Test Script for PiPlates
+#
+# https://pi-plates.com/daqc-users-guide/
+# https://pi-plates.com/downloads/DAQCplate%20Quick%20Reference%20Guide.pdf
+#
+# http://pi-plates.com/relayplate-users-guide
+# https://pi-plates.com/downloads/RELAYplateQuickReferenceGuide.pdf
+#
+# Autor: Walter Rothlin
+#
+# History:
+# 30-Nov-2024   Walter Rothlin      Initial Version
+# ------------------------------------------------------------------
+import piplates.DAQCplate                       as DAQC
+import piplates.RELAYplate                      as RELAY
+
+print('Hello')
--- Python_WaltisExamples/Code_01_UtilityScripts/printIP.py
+++ Python_WaltisExamples/Code_01_UtilityScripts/printIP.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 # ------------------------------------------------------------------
 # Name: printIP.py
@@ -6,67 +6,88 @@
 #
 # Description: Zeigt die IP-Adresse auf der Sense-Hat LED Matrix
 #
+# API HAT: https://pythonhosted.org/sense-hat/api/
+#
 # Autor: Walter Rothlin
 #
 # History:
-# 16-May-2023   Walter Rothlin      Added header to initial version
+# 01-Dec-2024   Walter Rothlin      Added header to initial version
 # ------------------------------------------------------------------
-
-
 import os
 import sys
 
 from sense_hat import SenseHat
 
-
 sense = SenseHat()
 
-
 print("\n\n")
-print("# Show IP Adresse (V1.0.0.0)")
+print("# Show IP Adresse (V1.0.0.1)")
 print("# --------------------------")
-name    = "BZU:"
-vorname = "Waiting for Joystick Event"
-displayMsg = "Hello " + name + " " + vorname
+sense.show_message('Pls wait....', text_colour=(0, 125, 125))
 
-print(displayMsg)
-sense.show_message(displayMsg, text_colour=(0, 125, 125))
+X = [255, 0, 0]  # Red
+O = [70, 70, 0]  # Yellow
 
+arrow_mark = [
+    O, O, O, X, O, O, O, O,
+    O, O, X, X, X, O, O, O,
+    O, X, X, X, X, X, O, O,
+    O, O, O, X, O, O, O, O,
+    O, O, O, X, O, O, O, O,
+    O, O, O, X, O, O, O, O,
+    O, O, O, X, O, O, O, O,
+    O, O, O, X, O, O, O, O
+]
 
-X = [255, 0, 0]   # Red
-O = [70, 70, 0]    # Yellow
+rotation = 0
+finished = False
+while not finished:
+    sense.set_pixels(arrow_mark)
+    event = sense.stick.wait_for_event(emptybuffer=True)
+    print(f'Joystick:{event.direction} {event.action}    Rotation:{rotation}')
+    if event.action == 'pressed':
+        if event.direction == "middle":
+            finished = True
+        else:
+            rotation += 90
+            rotation = rotation % 360
+            sense.set_rotation(rotation)
+
+X = [255, 0, 0]  # Red
+O = [70, 70, 0]  # Yellow
 
 question_mark = [
-O, O, O, X, X, O, O, O,
-O, O, X, O, O, X, O, O,
-O, O, O, O, O, X, O, O,
-O, O, O, O, X, O, O, O,
-O, O, O, X, O, O, O, O,
-O, O, O, X, O, O, O, O,
-O, O, O, O, O, O, O, O,
-O, O, O, X, O, O, O, O
+    O, O, O, X, X, O, O, O,
+    O, O, X, O, O, X, O, O,
+    O, O, O, O, O, X, O, O,
+    O, O, O, O, X, O, O, O,
+    O, O, O, X, O, O, O, O,
+    O, O, O, X, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, X, O, O, O, O
 ]
 
 finished = False
 while not finished:
     sense.set_pixels(question_mark)
-    
-    # wait for events, but delete all events happen before this call
+
     event = sense.stick.wait_for_event(emptybuffer=True)
-    print("The joystick was {} {}".format(event.action, event.direction))
-    if (event.direction == "right"):
-        finished = True
-    elif (event.direction == "left"):
-        myHostname = os.popen('/bin/hostname').read().strip()
-        print("  Hostname:" + myHostname + "::")
-        sense.show_message(str(myHostname), text_colour=(125, 125, 0))
+    print(f'Joystick:{event.direction} {event.action}')
+    if event.action == 'pressed':
+        if event.direction == 'middle':
+            finished = True
+        elif event.direction == 'left':
+            myHostname = os.popen('/bin/hostname').read().strip()
+            print(f'  Hostname:{myHostname}::')
+            sense.show_message(str(myHostname), text_colour=(125, 125, 0))
 
-    ipArray = os.popen('/bin/hostname -I').read().strip().split(" ")
-    for ip in ipArray:
-        ipStr = str(ip).strip('\r\n')
-        print("  IP:" + ipStr + "::")
-        sense.show_message(str(ipStr), text_colour=(125, 125, 125))
+        ipArray = os.popen('/bin/hostname -I').read().strip().split(" ")
+        for ip in ipArray:
+            ipStr = str(ip).strip('\r\n')
+            print(f'  IP:{ipStr}:: {len(ipStr)}')
+            if len(ipStr) < 20:
+                sense.show_message(str(ipStr), text_colour=(125, 125, 125))
 
-    sense.clear()   # clear LED matrix
+    sense.clear()  # clear LED matrix
 
 sense.show_message("Done & Closed!!!", text_colour=(0, 125, 125))
