4969a634b0cc2c9c5194da80b89fd87b63ed74a7
Walter Rothlin <46260915+walter-rothlin@users.noreply.github.com>
2025-06-17 07:40:52 +0200

    Update

--- Adressverwaltung/Adress_DB_Migration.sql
+++ Adressverwaltung/Adress_DB_Migration.sql
@@ -6,40 +6,9 @@
 -- 16-Jun-2025   Walter Rothlin      Initial Version
 -- ---------------------------------------------------------------------------------
 
--- ===========================================================================
--- Eigenes Schema kreieren und normalisieren
--- ===========================================================================
-DROP SCHEMA IF EXISTS `bzu_blf`;
-CREATE SCHEMA `bzu_blf`;
-
-SELECT SLEEP(1);  -- wait 1 sec, just to give a chance to set schema as default
-USE `bzu_blf`;
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
-
--- Table `Adressen` create
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `Adressen`;
-CREATE TABLE IF NOT EXISTS `Adressen` (
-  `ID`       INT         NOT NULL AUTO_INCREMENT,
-  `Vorname`  VARCHAR(45) NOT NULL,
-  `Nachname` VARCHAR(45) NOT NULL,
-  `Strasse`  VARCHAR(45) NULL,
-  `PLZ`      VARCHAR(20) NOT NULL,
-  `Ort`      VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`ID`));
-
--- Testdatensätze einfügen
--- -----------------------------------------------------
-INSERT INTO `Adressen` (`ID`, `Vorname`, `Nachname`, `Strasse`, `PLZ`, `Ort`) VALUES 
-(1, 'Walter', 'Rothlin', 'Peterliwiese 33', '8855', 'Wangen'),
-(2, 'Max', 'Meier', 'Etzelstr. 7', '8855', 'Wangen'),
-(3, 'Claudia', 'Müller', 'Hauptstr. 179a', '8853', 'Lachen'),
-(4, 'Maria', 'Bächtiger', 'Etzelstr. 7', '8853', 'Lachen'),
-(5, 'Fritz', 'Künzli', 'Rigiweg 55c', '8855', 'Nuolen');
 
 -- ---------------------------------------------------
 -- Entkoppeln von interner Sicht und Applikationssicht
